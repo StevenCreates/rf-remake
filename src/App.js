fee8641c14db2f79ec96327e74be967923d20ddb
Steven Hofheins <stevenisinsane@gmail.com>
2020-10-22 07:56:34 -0600

    added profile

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import LoginButton from "./components/LoginButton";
 import LogoutButton from "./components/LogoutButton";
+import Profile from "./components/Profile";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <header className='App-header'>
         <LoginButton />
         <LogoutButton />
+        <Profile />
       </header>
     </div>
   );
