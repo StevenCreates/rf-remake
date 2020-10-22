dbed4a42fdde5b703de182f96e7859d5a5769654
Steven Hofheins <stevenisinsane@gmail.com>
2020-10-22 07:55:00 -0600

    added logout button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
-import LoginButton from "./components/Button";
+import LoginButton from "./components/LoginButton";
+import LogoutButton from "./components/LogoutButton";
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
         <LoginButton />
+        <LogoutButton />
       </header>
     </div>
   );
