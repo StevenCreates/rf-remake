dee6bdbd51fd7281abd2cbee7db1ef4a630fa185
Steven Hofheins <stevenisinsane@gmail.com>
2020-10-25 13:40:24 -0600

    made styling changes to navbar and removed login button

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,12 @@
+import { AnimatePresence, motion } from "framer-motion";
 import React from "react";
 import styled from "styled-components";
 import MenuIcon from "../assets/MenuIcon";
+import CloseMenuIcon from "../assets/CloseMenuIcon";
 import LoginButton from "./LoginButton";
 import LogoutButton from "./LogoutButton";
 
-const NavOpen = styled.nav`
+const NavOpen = styled(motion.nav)`
   background: hotpink;
   width: 15vw;
   display: grid;
@@ -14,13 +16,19 @@ const NavOpen = styled.nav`
   margin-left: 1.5rem;
 `;
 
-const NavClosed = styled.nav`
+const NavClosed = styled(motion.nav)`
   background: hotpink;
-  width: 3vw;
-  padding: 0.2rem;
+  width: 2vw;
+  padding-left: 0.25rem;
+  padding-right: 0.25rem;
+  /* padding: 0.2rem; */
   margin-top: 1.5rem;
   margin-left: 1.5rem;
   border-radius: 10px;
+`;
+
+const IconStyled = styled(motion.div)`
+  width: 2vw;
 `;
 
 const NavBar = () => {
@@ -29,20 +37,34 @@ const NavBar = () => {
   const OpenSesame = () => {
     setOpen(true);
   };
+
+  const CloseSesame = () => {
+    setOpen(false);
+  };
   return (
     <>
       {!open && (
-        <NavClosed>
-          <div type='button' onClick={OpenSesame}>
-            <MenuIcon />
-          </div>
-        </NavClosed>
+        <AnimatePresence>
+          <NavClosed
+            initial={{ y: -100 }}
+            animate={{ y: 0 }}
+            exit={{ y: -100 }}>
+            <IconStyled type='button' onClick={OpenSesame}>
+              <MenuIcon />
+            </IconStyled>
+          </NavClosed>
+        </AnimatePresence>
       )}
       {open && (
-        <NavOpen>
-          <LoginButton />
-          <LogoutButton />
-        </NavOpen>
+        <AnimatePresence>
+          <NavOpen initial={{ y: -100 }} animate={{ y: 0 }} exit={{ y: -100 }}>
+            <IconStyled type='button' onClick={CloseSesame}>
+              <CloseMenuIcon />
+            </IconStyled>
+            {/* <LoginButton /> */}
+            <LogoutButton />
+          </NavOpen>
+        </AnimatePresence>
       )}
     </>
   );
