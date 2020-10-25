a23cb64627ffc4ec4e9b0ed76cbd7d8098904739
Steven Hofheins <stevenisinsane@gmail.com>
2020-10-25 13:40:09 -0600

    imported close icon svg

---
+++ src/assets/CloseMenuIcon.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+function CloseMenuIconß() {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      className='ionicon'
+      viewBox='0 0 512 512'>
+      <path
+        fill='none'
+        stroke='currentColor'
+        strokeMiterlimit='10'
+        strokeWidth='32'
+        d='M448 256c0-106-86-192-192-192S64 150 64 256s86 192 192 192 192-86 192-192z'></path>
+      <path
+        fill='none'
+        stroke='currentColor'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        strokeWidth='32'
+        d='M320 320L192 192m0 128l128-128'></path>
+    </svg>
+  );
+}
+
+export default CloseMenuIconß;
