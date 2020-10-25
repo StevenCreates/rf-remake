09494a76f0c068e3f69e6f8ed466f122b762e090
Steven Hofheins <stevenisinsane@gmail.com>
2020-10-25 08:01:44 -0600

    cleaned up appjs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from "react";
 import "./App.css";
-import LoginButton from "./components/LoginButton";
-import LogoutButton from "./components/LogoutButton";
 import NavBar from "./components/NavBar";
 import PieChart from "./components/PieChart";
 import Profile from "./components/Profile";
