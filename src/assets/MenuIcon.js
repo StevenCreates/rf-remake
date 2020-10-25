cd4504ca7a6124eaade3f236df2928320f454478
Steven Hofheins <stevenisinsane@gmail.com>
2020-10-25 08:48:25 -0600

    working on nav for logging in and out

---
+++ src/assets/MenuIcon.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function MenuIcon() {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      className='ionicon'
+      viewBox='0 0 512 512'>
+      <path
+        fill='none'
+        stroke='currentColor'
+        strokeLinecap='round'
+        strokeMiterlimit='10'
+        strokeWidth='32'
+        d='M80 160h352M80 256h352M80 352h352'></path>
+    </svg>
+  );
+}
+
+export default MenuIcon;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,50 @@
 import React from "react";
 import styled from "styled-components";
+import MenuIcon from "../assets/MenuIcon";
 import LoginButton from "./LoginButton";
 import LogoutButton from "./LogoutButton";
 
-const Nav = styled.div``;
+const NavOpen = styled.nav`
+  background: hotpink;
+  width: 15vw;
+  display: grid;
+  grid-template-rows: 1fr 1fr 1fr;
+  padding: 3px;
+  margin-top: 1.5rem;
+  margin-left: 1.5rem;
+`;
+
+const NavClosed = styled.nav`
+  background: hotpink;
+  width: 3vw;
+  padding: 0.2rem;
+  margin-top: 1.5rem;
+  margin-left: 1.5rem;
+  border-radius: 10px;
+`;
 
 const NavBar = () => {
+  const [open, setOpen] = React.useState(false);
+
+  const OpenSesame = () => {
+    setOpen(true);
+  };
   return (
-    <Nav>
-      <LoginButton />
-      <LogoutButton />
-    </Nav>
+    <>
+      {!open && (
+        <NavClosed>
+          <div type='button' onClick={OpenSesame}>
+            <MenuIcon />
+          </div>
+        </NavClosed>
+      )}
+      {open && (
+        <NavOpen>
+          <LoginButton />
+          <LogoutButton />
+        </NavOpen>
+      )}
+    </>
   );
 };
 
