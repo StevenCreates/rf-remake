6c1583a0a5c9de9d7027674ea01f9dcea227fd50
Steven Hofheins <stevenisinsane@gmail.com>
2020-10-25 14:50:49 -0600

    added selctor for different dashboards

---
+++ src/components/DashboardChanger.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const DashboardChanger = () => {
+  return (
+    <select>
+      <option value=''>Registration Dashboard</option>
+      <option value=''></option>
+    </select>
+  );
+};
+
+export default DashboardChanger;
