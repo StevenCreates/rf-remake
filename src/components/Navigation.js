b5818147e6b6e73b6d194ba37071062cffd4d278
Steven Hofheins <stevenisinsane@gmail.com>
2020-10-25 21:19:15 -0600

    added more styling and the search svg

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,25 +1,56 @@
 import React from "react";
 import styled from "styled-components";
+import SearchIcon from "../assets/SearchIcon";
 
 const Navigation = () => {
   return (
-    <>
+    <NavContainer>
       <FirstNav>
-        <SearchItems>Search</SearchItems>
-        <SearchItems>Dashboard</SearchItems>
+        <SearchItems>
+          <IconStyled>
+            <SearchIcon />
+          </IconStyled>
+          Search
+        </SearchItems>
+        <SearchItems>Recents</SearchItems>
       </FirstNav>
       <Nav>
-        <NavItems>Employees</NavItems>
-        <NavItems>Employees</NavItems>
-        <NavItems>Employees</NavItems>
+        <NavItems>My Nav</NavItems>
+        <NavItems>Work Flows</NavItems>
+        <NavItems>Registration</NavItems>
+        <NavItems>Content</NavItems>
+        <NavItems>Meetings</NavItems>
       </Nav>
-    </>
+    </NavContainer>
   );
 };
 
-const FirstNav = styled.ul``;
-const SearchItems = styled.li``;
-const Nav = styled.ul``;
+const FirstNav = styled.ul`
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+`;
+const SearchItems = styled.li`
+  font-size: 20px;
+  font-weight: 700;
+`;
+const Nav = styled.ul`
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+`;
 const NavItems = styled.li``;
+
+const NavContainer = styled.div`
+  padding-left: 1rem;
+  color: #ffffff;
+`;
+
+const IconStyled = styled.div`
+  color: #ffffff;
+  width: 1.5rem;
+  display: inline-block;
+  padding-right: 0.5rem;
+`;
 
 export default Navigation;
