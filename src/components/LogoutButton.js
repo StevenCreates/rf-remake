f39a02c6e59f0105643ad2ffc2ad363239e5acd5
Steven Hofheins <stevenisinsane@gmail.com>
2020-10-25 20:10:06 -0600

    added a little stlying to logout button

--- src/components/LogoutButton.js
+++ src/components/LogoutButton.js
@@ -1,13 +1,19 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import styled from "styled-components";
 
 const LogoutButton = () => {
   const { logout } = useAuth0();
 
+  const StyledButton = styled.button`
+    margin: 1rem;
+    height: 3rem;
+  `;
+
   return (
-    <button onClick={() => logout({ returnTo: window.location.origin })}>
+    <StyledButton onClick={() => logout({ returnTo: window.location.origin })}>
       Log Out
-    </button>
+    </StyledButton>
   );
 };
 
