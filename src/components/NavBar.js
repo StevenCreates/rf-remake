25d2f7dc96e2d8b7c031a6a03b3dbb4072eb051f
Steven Hofheins <stevenisinsane@gmail.com>
2020-10-26 21:09:44 -0600

    removed unused code

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,6 @@
 import { AnimatePresence, motion } from "framer-motion";
 import React from "react";
 import styled from "styled-components";
-import MenuIcon from "../assets/MenuIcon";
-import CloseMenuIcon from "../assets/CloseMenuIcon";
-import LoginButton from "./LoginButton";
 import LogoutButton from "./LogoutButton";
 import RainFocusLogo from "../assets/RainFocusLogo";
 import Navigation from "./Navigation";
@@ -17,58 +14,20 @@ const NavOpen = styled(motion.nav)`
   height: 100vh;
 `;
 
-const NavClosed = styled(motion.nav)`
-  background: #ed1a5d;
-  width: 2vw;
-  padding-left: 0.25rem;
-  padding-right: 0.25rem;
-  /* padding: 0.2rem; */
-  margin-top: 1.5rem;
-  border-radius: 10px;
-  height: 2rem;
-`;
-
-const IconStyled = styled(motion.div)`
-  width: 2vw;
-`;
-
 const NavBar = () => {
-  const [open, setOpen] = React.useState(false);
-
-  const OpenSesame = () => {
-    setOpen(true);
-  };
-
-  const CloseSesame = () => {
-    setOpen(false);
-  };
   return (
     <>
-      {open && (
-        <AnimatePresence>
-          <NavClosed
-            initial={{ y: -100 }}
-            animate={{ y: 0 }}
-            exit={{ y: -100 }}>
-            <IconStyled type='button' onClick={OpenSesame}>
-              <MenuIcon />
-            </IconStyled>
-          </NavClosed>
-        </AnimatePresence>
-      )}
-      {!open && (
-        <AnimatePresence>
-          <NavOpen initial={{ y: -100 }} animate={{ y: 0 }} exit={{ y: -100 }}>
-            <div>
-              <RainFocusLogo />
-            </div>
-            <div>
-              <Navigation />
-            </div>
-            <LogoutButton />
-          </NavOpen>
-        </AnimatePresence>
-      )}
+      <AnimatePresence>
+        <NavOpen initial={{ y: -100 }} animate={{ y: 0 }} exit={{ y: -100 }}>
+          <div>
+            <RainFocusLogo />
+          </div>
+          <div>
+            <Navigation />
+          </div>
+          <LogoutButton />
+        </NavOpen>
+      </AnimatePresence>
     </>
   );
 };
